6d1a9b791812600b3504562c979aaaf8d7deb868
Maceo <marcosmaceo2013@gmail.com>
2018-07-09 13:14:10 -0400

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_08_153102) do
+ActiveRecord::Schema.define(version: 2018_07_09_163748) do
 
   create_table "sitios", force: :cascade do |t|
     t.string "direccion"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_07_08_153102) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
